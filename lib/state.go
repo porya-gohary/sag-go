32d917c202435ccd03e0f511b771af38ef2e0e65
porya-gohary <gohary.porya@gmail.com>
2022-04-25 13:51:49 +0200

    improve state definition

--- lib/state.go
+++ lib/state.go
@@ -1,6 +1,61 @@
 package lib
 
+import (
+	"strconv"
+)
+
 type State struct {
-	Name string
-	Age  int
+	Index int
+	Availibility Interval
+	ScheduledJobs JobSet
+	EarliestPendingRelease Time
+}
+
+type StateStorage map[string]State
+
+// functions for state
+func NewState(index int,finishTime Interval,j JobSet,earliestRelease Time) State {
+
+	return State{
+		Index: index,
+		Availibility: finishTime,
+		ScheduledJobs: j,
+		EarliestPendingRelease: earliestRelease,
+	}
+}
+
+func (s State) GetName() string {
+	return "S"+strconv.Itoa(s.Index)
+}
+
+func (s State) ID() string {
+	return s.GetName()
+}
+
+func (s State) String() string {
+	return s.GetName()+"\n"+s.Availibility.String()+"\n{"+s.ScheduledJobs.AbstractString()+"}\n"+s.EarliestPendingRelease.String()
+}
+
+// functions for state storage
+
+func NewStateStorage() *StateStorage {
+	return &StateStorage{}
+}
+
+func (s *StateStorage) Initialize() {
+	s0 := NewState(0,Interval{Start: 0, End: 0}, JobSet{}, Time(0))
+	s.AddState(s0)
+}
+
+func (s StateStorage) AddState (st State){
+	s[st.ID()] = st
+}
+
+func (s StateStorage) String() string{
+	var str string
+	for _,v := range s {
+		str += v.String() + "\n---------\n"
+	}
+	return str
+
 }
